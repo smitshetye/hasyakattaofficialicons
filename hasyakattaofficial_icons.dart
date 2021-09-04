c06ad1e3650f26cd6c8021a5909b0dea0ea7c674
Smit Bhaskar Shetye <smitshetye31@gmail.com>
2021-09-04 22:27:08 +0530

    Created Hasya Katta Official Icons.
    Created Hasya Katta Official Icons.

---
+++ hasyakattaofficial_icons.dart
@@ -0,0 +1,17 @@
+// Place fonts/hasyakattaofficial.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: hasyakattaofficial
+//      fonts:
+//       - asset: fonts/hasyakattaofficial.ttf
+import 'package:flutter/widgets.dart';
+
+class Hasyakattaofficial {
+  Hasyakattaofficial._();
+
+  static const String _fontFamily = 'hasyakattaofficial';
+
+  static const IconData hasya_katta = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData hasya_katta_1 = IconData(0xe901, fontFamily: _fontFamily);
+}
